ab5c2ae1be7a4b7e9db211390d7ea14a2cacbbc5
Llorx <dallorx@gmail.com>
2017-06-30 12:55:37 +0200

    Pushing openweathermap as it has more nodes

--- WeatherReadings.js
+++ WeatherReadings.js
@@ -1,10 +1,6 @@
 var http = require("http");
 
-var key = require("./credentials").mesowest.key;
-
-/*
-// openweathermap updates data each 2 hours and has half the number of nodes than mesowest
-// No need to divide by quadrants as they limit by API call count, not by number of stations queried
+var key = require("./credentials").openweather.key;
 function getWeather(publish) {
     http.get("http://api.openweathermap.org/data/2.5/box/city?bbox=-180,90,180,-90,20&&cluster=no&appid=" + key, (res) => {
         var data = "";
@@ -13,36 +9,63 @@ function getWeather(publish) {
         });
         res.on("end", () => {
             try {
-                processWeather(JSON.parse(data).list, publish);
+                data = JSON.parse(data);
             } catch (e) {
             }
-            setTimeout(getWeather.bind(null, publish), 30*60*1000);
+            processWeather(data, publish);
         });
     });
 }
 
-var previousList = {};
+// Do not keep track of changes. Simply push them: var previousList = {};
 
-function processWeather(list, publish) {
-    list.forEach((w) => {
-        if (previousList[w.id] != w.dt) {
-            previousList[w.id] = w.dt;
-            publish({
-                name: w.name,
-                coords: w.coord,
-                environment: w.main,
-                wind: w.wind,
-                rain_volume: w.rain || 0,
-                snow_volume: w.snow || 0,
-                clouds_coverage: (w.clouds && w.clouds.hasOwnProperty("today")) ? w.clouds.today + "%" : "unknown"
-            });
-        }
-    });
-}*/
+function processWeather(data, publish) {
+    var topublish = [];
+    try {
+        data.list.forEach((w) => {
+            /*if (previousList[w.id] != w.dt) {
+                previousList[w.id] = w.dt;*/
+            topublish.push({
+                    id: w.id,
+                    name: w.name,
+                    coords: {
+                        lon: w.coord.Lon,
+                        lat: w.coord.Lat
+                    },
+                    environment: w.main,
+                    wind: w.wind,
+                    rain_volume: w.rain || 0,
+                    snow_volume: w.snow || 0,
+                    clouds_coverage: (w.clouds && w.clouds.hasOwnProperty("today")) ? w.clouds.today : "unknown"
+                });
+            //}
+        });
+    } catch (e) {
+    }
+    if (topublish.length > 0) {
+        // Shuffle to publish data all around the map instead of focusing on spots
+        shuffle(topublish);
 
-// Divide the api calls by quadrants (100 quadrants)
+        // Publish them until the next call in 10 minutes instead of pushing a big bulk
+        publishStations(topublish, (10 * 60 * 1000) / topublish.length, publish, () => {
+            getWeather(publish);
+        });
+    } else {
+        getWeather(publish);
+    }
+}
+
+module.exports = getWeather;
+
+/*
+// USA only
+var key = require("./credentials").mesowest.key;
+// Divide the api calls by quadrants
 var quadrants = [];
-var divisor = 10;
+//var divisor = 4; // 16 quadrants
+//var divisor = 10; // 100 quadrants
+//var divisor = 20; // 400 quadrants
+var divisor = 1; // 1 quadrant
 
 var lonmin = -180;
 var lonmax = 180;
@@ -67,27 +90,30 @@ do {
     do {
         prelat = lat;
         lat += latdif;
-        quadrants.push([prelon == lonmin ? prelon + 0.001 : prelon, prelat == latmin ? prelat + 0.001 : prelat, lon == lonmax ? lon - 0.001 : lon, lat == latmax ? lat - 0.001 : lat].join(",")); // Decimal fix as mesowest don't accepts minimum and maximum lon and lat
+        quadrants.push([prelon <= lonmin ? prelon + 0.001 : prelon, prelat <= latmin ? prelat + 0.001 : prelat, lon >= lonmax ? lon - 0.001 : lon, lat >= latmax ? lat - 0.001 : lat].join(",")); // Decimal fix as mesowest don't accepts minimum and maximum lon and lat
     } while (lat < latmax);
 } while (lon < lonmax);
 
-var currentQuadrant = 0;
+// The amount of stations I can query per hour. API limited
+var hour_limit = 100000;
 
-var looptimeout = (30 * 60 * 1000) / quadrants.length; // Loop throgh all the quadrants each 30 minutes (around 40k stations are queried and we have a 100k station limit per hour).
+// The time publishing one station should last to not surpass the limit
+var perstation = Math.ceil((60 * 60 * 1000) / hour_limit);
+
+// Add 5% more time per station to not be so close to the limit
+perstation += Math.ceil(perstation * 0.05);
 
 var reservedquadrants = [];
 setInterval(() => {
     // Add the reserved quadrants for requery each 6 hours, just in case they are updated
     quadrants = quadrants.concat(reservedquadrants);
-    looptimeout = (30 * 60 * 1000) / quadrants.length;
-}, 6 * 60 * 60 * 1000);
+}, 3 * 60 * 60 * 1000);
 
-function getWeather(publish) {
-    if (!quadrants[currentQuadrant]) {
-        currentQuadrant = 0;
-    }
-    console.log(quadrants[currentQuadrant]);
-    http.get("http://api.mesowest.net/v2/stations/latest?status=active&bbox=" + quadrants[currentQuadrant] + "&token=" + key, (res) => { // TODO, change -17 = -179
+var cachedQuadrants = [];
+
+function nextQuadrant(publish) {
+    var quadrant = quadrants.shift(); // Remove from bottom stack
+    http.get("http://api.mesowest.net/v2/stations/latest?status=active&bbox=" + quadrant + "&token=" + key, (res) => {
         var data = "";
         res.on("data", (c) => {
             data += c;
@@ -95,103 +121,148 @@ function getWeather(publish) {
         res.on("end", () => {
             try {
                 data = JSON.parse(data);
-                console.log(data.SUMMARY);
-                if (data.SUMMARY.RESPONSE_CODE == -1) { // No stations found for this request
-                    // Remove the quadrant and reserve it to query it later, just in case new stations are added
-                    reservedquadrants.push(quadrants.splice(currentQuadrant, 1)[0]);
-                    if (currentQuadrant >= quadrants.length) {
-                        currentQuadrant = 0;
-                    }
-                    looptimeout = (30 * 60 * 1000) / quadrants.length;
-                    return getWeather(publish);
-                } else {
-                    processWeather(data, publish);
-                }
             } catch (e) {
-                console.log(e);
             }
-            currentQuadrant++;
-            if (currentQuadrant >= quadrants.length) {
-                currentQuadrant = 0;
+            if (data.SUMMARY.RESPONSE_CODE == -1) { // No stations found for this request
+                // Remove the quadrant and reserve it to query it later, just in case new stations are added
+                reservedquadrants.push(quadrant);
+                return nextQuadrant(publish);
+            } else {
+                if (data.STATION) {
+                    processWeather(data, publish);
+                } else {
+                    nextQuadrant(publish);
+                }
             }
-            setTimeout(getWeather.bind(null, publish), looptimeout);
+            quadrants.push(quadrant); // Push to top stack
         });
     });
 }
 
 var list = {};
 
+var processing = false;
+var processQeue = [];
 function processWeather(data, publish) {
-    var units = data.UNITS;
-    var topost = [];
-    data.STATION.forEach((station) => {
-        var variables = {};
-        if (!list.hasOwnProperty(station.ID)) {
-            list[station.ID] = {
-                variables: {}
-            }
-        }
-        var oldStation = list[station.ID];
-        var oldVariables = oldStation.variables;
-        for (var vartype in station.SENSOR_VARIABLES) {
-            if (!oldVariables.hasOwnProperty(vartype)) {
-                oldVariables[vartype] = {};
-            }
-            var oldvar = oldVariables[vartype];
-            var unit = units.hasOwnProperty(vartype) ? units[vartype] : null;
-            var vars = station.SENSOR_VARIABLES[vartype];
-            var varnames = Object.keys(vars);
-            var v = [];
-            varnames.forEach((varname) => {
-                if (station.OBSERVATIONS.hasOwnProperty(varname)) {
-                    var obs = station.OBSERVATIONS[varname];
-                    var t = new Date(obs.date_time).getTime();
-                    if (!oldvar.hasOwnProperty(varname) || oldvar[varname] != obs.date_time) {
-                        v.push({
-                            value: obs.value,
-                            name: varname
-                        });
-                    }
-                    oldvar[varname] = t;
-                }
-            });
-            if (v.length > 0) {
-                if (varnames.length == 1) {
-                    v = v[0].value;
-                }
-                variables[vartype] = {
-                    value: v,
-                    unit
-                };
-            }
-        }
-        if (Object.keys(variables).length > 0) {
-            topost.push({
-                id: station.ID,
-                name: station.NAME,
-                timezone: station.TIMEZONE,
-                coords: {
-                    lon: Number(station.LONGITUDE),
-                    lat: Number(station.LATITUDE),
-                    alt: Number(station.ELEVATION)
-                },
-                data: variables
-            });
-        }
-    });
+    if (processing) {
+        processQeue.push(data);
+        return;
+    }
+    processing = true;
+    if (processQeue.length == 0) {
+        // Preload a quadrant
+        nextQuadrant(publish);
+    }
+    if (data && data.STATION && data.STATION.length > 0) {
+        var units = data.UNITS || {};
+        var topost = [];
 
-    // Stream them along the next looptimeout time (18 seconds right now) so are not sent in bulk
-    if (topost.length > 0) {
-        var timeout = looptimeout / topost.length;
-        var next = () => {
-            var st = topost.shift();
-            if (st) {
-                publish(st);
-                setTimeout(next, timeout);
+        // Find updated variables        
+        data.STATION.forEach((station) => {
+            try {
+                var variables = {};
+                if (!list.hasOwnProperty(station.ID)) {
+                    list[station.ID] = {
+                        variables: {}
+                    }
+                }
+                var oldStation = list[station.ID];
+                var oldVariables = oldStation.variables;
+                for (var vartype in station.SENSOR_VARIABLES) {
+                    if (!oldVariables.hasOwnProperty(vartype)) {
+                        oldVariables[vartype] = {};
+                    }
+                    var oldvar = oldVariables[vartype];
+                    var unit = units.hasOwnProperty(vartype) ? units[vartype] : null;
+                    var vars = station.SENSOR_VARIABLES[vartype];
+                    var varnames = Object.keys(vars);
+                    var v = [];
+                    varnames.forEach((varname) => {
+                        if (station.OBSERVATIONS.hasOwnProperty(varname)) {
+                            var obs = station.OBSERVATIONS[varname];
+                            var t = new Date(obs.date_time).getTime();
+                            if (!oldvar.hasOwnProperty(varname) || oldvar[varname] != obs.date_time) {
+                                v.push({
+                                    value: obs.value,
+                                    name: varname
+                                });
+                            }
+                            oldvar[varname] = t;
+                        }
+                    });
+                    if (v.length > 0) {
+                        if (varnames.length == 1) {
+                            v = v[0].value;
+                        }
+                        variables[vartype] = {
+                            value: v,
+                            unit
+                        };
+                    }
+                }
+                if (Object.keys(variables).length > 0) {
+                    topost.push({
+                        id: station.ID,
+                        name: station.NAME,
+                        timezone: station.TIMEZONE,
+                        coords: {
+                            lon: Number(station.LONGITUDE),
+                            lat: Number(station.LATITUDE),
+                            alt: Number(station.ELEVATION)
+                        },
+                        data: variables
+                    });
+                }
+            } catch (e) {
             }
+        });
+
+        // Stream the stations that have updated variables along the theorical duration of this quadrant
+        if (topost.length > 0) {
+            // Shuffle to publish data all around the map
+            shuffle(topost);
+            var timeoutPerStation = data.STATION.length * perstation / topost.length;
+            publishStations(topost, timeoutPerStation, publish, () => {
+                processing = false;
+                if (processQeue.length > 0) {
+                    processWeather(processQeue.shift(), publish);
+                }
+            });
         }
-        next();
+    } else {
+        processing = false;
+        if (processQeue.length > 0) {
+            processWeather(processQeue.shift(), publish);
+        }
     }
 }
 
-module.exports = getWeather;
+module.exports = nextQuadrant;*/
+
+function publishStations(stations, timeout, publish, end) {
+    var interval = setInterval(() => {
+        if (stations.length > 0) {
+            publish(stations.shift());
+        } else {
+            clearInterval(interval);
+            end();
+        }
+    }, timeout);
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+}
